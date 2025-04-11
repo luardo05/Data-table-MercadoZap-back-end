5330f97cfed331fee887f7757f0853140e88f0d8
Luiz Eduardo de Almeida <101152317+luardo05@users.noreply.github.com>
2025-04-11 09:30:57 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const userRoutes = require('./routes/userRoutes');
+const plantationRoutes = require('./routes/plantationRoutes');
+const productRoutes = require('./routes/productRoutes');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+// Middleware para parse de JSON
+app.use(express.json());
+
+app.use(cors());
+
+// Conectando ao MongoDB
+mongoose.connect('mongodb://localhost:27017/mydatabasenew', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Conectado ao MongoDB!');
+}).catch(err => {
+    console.log('Erro ao conectar ao MongoDB:', err);
+});
+
+// Usando rotas
+app.use('/api/users', userRoutes);
+app.use('/api/plantations', plantationRoutes);
+app.use('/api/products', productRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
